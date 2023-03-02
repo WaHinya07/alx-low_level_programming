d941855f988423012cc11373d28a9fe76f27a7dc
angela <angelawmwaura@gmail.com>
2023-03-02 12:25:16 -0800

    1337 Mozart

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * leet - function encodes a string into 1337
+ * @str: string to be encoded
+ * Return: encoded string
+ */
+char *leet(char *str)
+{
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == s1[j])
+			{
+				str[i] = s2[j];
+			}
+		}
+	}
+	return (str);
+}
