c1391015b4e2997057d61ef2a5018cc2e36ff2f4
angela <angelawmwaura@gmail.com>
2023-03-06 09:56:57 -0800

    Locate me, or else nullify

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - Function locares a character in a string
+ * @s: The string to be tested
+ * @c : The character to be located
+ * Return: The character in question, otherwise, 0
+ */
+char *_strchr(char *s, char c)
+{
+	for (; ; s++)
+	{
+		if (*s == c)
+			return (s);
+		if (*s == 0)
+			return (NULL);
+	}
+	return (NULL);
+}
