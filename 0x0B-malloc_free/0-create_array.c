cda3d57d88661e173857e37cede3bd75f91bbd6d
angela <angelawmwaura@gmail.com>
2023-03-13 12:02:19 -0700

    Create array and don't forget to allocate memory

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * create_array - function creates an array of chars and
+ * initializes it with a specific character
+ * @size: size of the array
+ * @c: specific character to be initialized
+ * Return: NULL if size is 0, a pointer to the array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *my_array;
+	unsigned int i;
+
+	my_array = malloc(sizeof(char) * size);
+
+	if (size == 0)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		my_array[i] = c;
+	}
+	return (my_array);
+}
