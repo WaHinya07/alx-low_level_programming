405cbef22707795e46a42db3275e3930a6a18464
angela <angelawmwaura@gmail.com>
2023-03-20 18:40:51 -0700

    function-like macro SUM for x and y

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,14 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * file: 4-sum.h
+ * Description: A function-like macro SUM(x, y)
+ * that computes the sum of x and y
+ * @x: first number to be added
+ * @y: second number to be added
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
