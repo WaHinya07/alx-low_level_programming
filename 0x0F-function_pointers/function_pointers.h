b551b0cd99231c0b4615602df3d38a8ffeeac095
angela <angelawmwaura@gmail.com>
2023-03-22 12:44:28 -0700

    fUNCTION POINTERS

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -6,10 +6,6 @@
  * Description: header file with prototypes for all functions
  * to be used for this project
  */
-
-#include <stdlib.h>
-
-int _putchar(char);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
