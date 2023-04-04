31cc8a496fa4b9569e8a6845b53cb82552b6f9b0
angela <angelawmwaura@gmail.com>
2023-04-04 12:29:15 -0700

    updated correction, return nth node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,13 +13,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int count;
 
-	if (head == NULL)
-	{
-		return (NULL);
-	}
-
 	for (count = 0; count < index; count++)
 	{
+		if (head == NULL)
+			return (NULL);
+
 		head = head->next;
 	}
 	return (head);
