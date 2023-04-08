0a43ec208a7634392959a9fa1793ab6238c03c14
angela <angelawmwaura@gmail.com>
2023-04-08 07:41:55 -0700

    unmalloced print binary file

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,20 +8,9 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int b_num;
-	
-	b_num = 1UL << (sizeof(unsigned long int) * 8 - 1);
-
-	while (b_num > 0)
+	if (n > 1)
 	{
-		if ((n & b_num) == 0)
-		{
-			putchar('0');
-		}
-		else
-		{
-			putchar('1');
-		}
-		b_num >>= 1;
+		print_binary(n >> 1);
 	}
+	_putchar((n & 1) + '0');
 }
