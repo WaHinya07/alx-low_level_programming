1edf4eccf9632d919a5705974b25d7f5dcfb0094
angela <angelawmwaura@gmail.com>
2023-04-08 11:45:49 -0700

    get bit crrection

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,5 +11,16 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	return ((n >> index) & 1);
+	if (index >= (sizeof(unsigned long int) * 8))
+	{
+		return (-1);
+	}
+	if ((n & 1UL << index) != 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
