3884baf3aab01105e4ba365996b9fb832c1a388b
angela <angelawmwaura@gmail.com>
2023-04-10 05:12:30 -0700

    create file with write and read permissions

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * create_file - Function that creates a file
+ * @filename: The name of the file to be created
+ * @text_content: Null terminated string to be written in the file
+ * Description: The created file must have those permissions: rw-------
+ * If the file already exists, do not change the permissions.
+ * if the file already exists, truncate it
+ * Return: 1 on success -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, bytes_written, len = 0;
+	mode_t mode = S_IRUSR | S_IWUSR;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+		len++;
+	}
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC | mode);
+	bytes_written = write(fd, text_content, len);
+
+	if (fd == -1 || bytes_written == -1)
+	{
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
