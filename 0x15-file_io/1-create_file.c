1e40f72f4749d1ed525da63135e190166e870a4a
angela <angelawmwaura@gmail.com>
2023-04-10 12:46:23 -0700

    Create a file with read and write permissions

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len];)
 		len++;
 	}
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	bytes_written = write(fd, text_content, len);
 
 	if (fd == -1 || bytes_written == -1)
