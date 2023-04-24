e6f064bb0a9465ea9b99e2d49c89a293d0b3a763
angela <angelawmwaura@gmail.com>
2023-04-24 03:08:32 -0700

    64bit program, hello holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,20 @@
-section .data
-    msg db 'Hello, Holberton', 0x0A ;
-message to print, terminated with a new line character (0x0A)
+extern printf
 
-section .text
-    global _start
+	section .data
+msg:	db	"Hello, Holberton", 0
+fmt:	db	"%s", 10, 0
 
-_start:
-    ; write the message to stdout
-    mov eax, 4          ; system call for write
-    mov ebx, 1          ; file descriptor for stdout
-    mov ecx, msg        ; message to print
-    mov edx, 16         ; message length in bytes
-    int 0x80            ; invoke the system call
+	section	.text
 
-    ; exit with status code 0
-    mov eax, 1          ; system call for exit
-    xor ebx, ebx        ; status code (0 = success)
-    int 0x80            ; invoke the system call
+	global main
 
+main:	push	rbp
+	mov rsi, msg
+	mov rdi, fmt
+	mov rax, 0
+	call printf
+
+	pop	rbp
+
+	mov	rax, 0
+	ret
